bfc24c9da0bd0f19da3f3aacb5c550dc81473383
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2020-08-26 00:13:56 -0700

    refactor

--- src/term.rs
+++ src/term.rs
@@ -7,10 +7,11 @@ fn fresh() -> String {
     let mut c = COUNTER.lock().unwrap();
     let r = *c;
     *c += 1;
-    format!("g#{}", r)
+    format!("#{}", r)
 }
 
-fn fresh_tvar() -> usize {
+// TODO: reclaim unused mvars
+pub fn fresh_mvar() -> usize {
     use std::sync::{Arc, Mutex};
     static COUNTER: Lazy<Arc<Mutex<usize>>> = Lazy::new(Default::default);
     let mut c = COUNTER.lock().unwrap();
@@ -21,31 +22,62 @@ fn fresh_tvar() -> usize {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Type {
-    Var(usize),
+    Fvar(String),
+    Bvar(usize),
     Arrow(Box<Type>, Box<Type>),
-    Base(String),
+    Mvar(usize),
 }
 
 impl Type {
-    pub fn subst(&mut self, i: usize, t: &Type) {
+    fn dummy() -> Self {
+        Self::Bvar(0xdeadbeef)
+    }
+
+    fn instantiate(&mut self, mids: &[usize]) {
         match self {
-            Self::Var(j) => {
+            Self::Fvar(_) => {}
+            Self::Bvar(i) => {
+                *self = Self::Mvar(mids[*i]);
+            }
+            Self::Arrow(t1, t2) => {
+                t1.instantiate(mids);
+                t2.instantiate(mids);
+            }
+            Self::Mvar(_) => todo!("mvar in Type::instantiate"),
+        }
+    }
+
+    pub fn subst_meta(&mut self, i: usize, t: &Type) {
+        match self {
+            Self::Fvar(_) => {}
+            Self::Bvar(_) => {}
+            Self::Mvar(j) => {
                 if i == *j {
                     *self = t.clone();
                 }
             }
             Self::Arrow(t1, t2) => {
-                t1.subst(i, t);
-                t2.subst(i, t);
+                t1.subst_meta(i, t);
+                t2.subst_meta(i, t);
             }
-            Self::Base(_) => {}
         }
     }
+}
 
-    fn apply_subst(&mut self, subst: &TypeSubst) {
-        for (i, t) in &subst.0 {
-            self.subst(*i, t);
+#[derive(Debug, Clone)]
+pub struct TypeScheme(usize, Type);
+
+impl TypeScheme {
+    pub fn instantiate(&self) -> Type {
+        let mut t = self.1.clone();
+        if self.0 > 0 {
+            t.instantiate(
+                &std::iter::repeat_with(fresh_mvar)
+                    .take(self.0)
+                    .collect::<Vec<_>>(),
+            );
         }
+        t
     }
 }
 
@@ -68,29 +100,6 @@ impl Term {
         }
     }
 
-    /// self.open_subst(m) == [m/x][x/0]self (x#self) == [m/0]self
-    pub fn open_subst(&mut self, m: &Term) {
-        self.open_subst_at(m, 0)
-    }
-
-    fn open_subst_at(&mut self, m: &Term, level: usize) {
-        match self {
-            Self::Fvar(_, _) => {}
-            Self::Bvar(_, i) => {
-                if *i == level {
-                    *self = m.clone();
-                }
-            }
-            Self::Abs(_, n) => {
-                n.open_subst_at(m, level + 1);
-            }
-            Self::App(_, m1, m2) => {
-                m1.open_subst_at(m, level);
-                m2.open_subst_at(m, level);
-            }
-        }
-    }
-
     /// self.open(x) == [x/0]self
     pub fn open(&mut self, name: &str) {
         self.open_at(name, 0)
@@ -101,7 +110,7 @@ impl Term {
             Self::Fvar(_, _) => {}
             Self::Bvar(t, i) => {
                 if *i == level {
-                    *self = Self::Fvar(std::mem::replace(t, Type::Var(0)), name.to_owned());
+                    *self = Self::Fvar(std::mem::replace(t, Type::dummy()), name.to_owned());
                 }
             }
             Self::Abs(_, n) => {
@@ -123,7 +132,7 @@ impl Term {
         match self {
             Self::Fvar(t, ref x) => {
                 if name == x {
-                    *self = Self::Bvar(std::mem::replace(t, Type::Var(0)), level);
+                    *self = Self::Bvar(std::mem::replace(t, Type::dummy()), level);
                 }
             }
             Self::Bvar(_, _) => {}
@@ -198,28 +207,29 @@ impl Term {
         }
     }
 
-    pub fn subst_type(&mut self, i: usize, t: &Type) {
+    pub fn subst_type_meta(&mut self, i: usize, t: &Type) {
         match self {
-            Self::Fvar(u, _) => u.subst(i, t),
+            Self::Fvar(u, _) => u.subst_meta(i, t),
             Self::Bvar(u, _) => {
-                u.subst(i, t);
+                u.subst_meta(i, t);
             }
             Self::Abs(u, m) => {
-                u.subst(i, t);
-                m.subst_type(i, t);
+                u.subst_meta(i, t);
+                m.subst_type_meta(i, t);
             }
             Self::App(u, m1, m2) => {
-                u.subst(i, t);
-                m1.subst_type(i, t);
-                m2.subst_type(i, t);
+                u.subst_meta(i, t);
+                m1.subst_type_meta(i, t);
+                m2.subst_type_meta(i, t);
             }
         }
     }
 
-    fn apply_subst_type(&mut self, subst: &TypeSubst) {
-        for (i, t) in &subst.0 {
-            self.subst_type(*i, t);
-        }
+    /// self.open_subst(m) == [m/x][x/0]self (for fresh x) == [m/0]self
+    pub fn open_subst(&mut self, m: &Term) {
+        let x = fresh();
+        self.open(&x);
+        self.subst(&x, m);
     }
 
     pub fn head(&self) -> &Self {
@@ -235,27 +245,35 @@ impl Term {
 struct TypeSubst(Vec<(usize, Type)>);
 
 impl TypeSubst {
+    // should we put this method in `impl Type` instead?
+    fn apply_type(&self, t: &mut Type) {
+        for (i, u) in &self.0 {
+            t.subst_meta(*i, u);
+        }
+    }
+
+    fn apply_term(&self, m: &mut Term) {
+        for (i, t) in &self.0 {
+            m.subst_type_meta(*i, t);
+        }
+    }
+
     fn unify(&mut self, t1: &Type, t2: &Type) {
+        if t1 == t2 {
+            return;
+        }
         match (t1, t2) {
             (Type::Arrow(t11, t12), Type::Arrow(t21, t22)) => {
                 self.unify(t11, t21);
                 self.unify(t12, t22);
             }
-            (Type::Base(name1), Type::Base(name2)) => {
-                if name1 != name2 {
-                    unimplemented!("unify {:?} {:?}", t1, t2);
-                }
-            }
-            (Type::Var(i), t) | (t, Type::Var(i)) => {
-                if &Type::Var(*i) == t {
-                    return;
-                }
+            (Type::Mvar(i), t) | (t, Type::Mvar(i)) => {
                 if self.0.iter().find(|(j, _)| j == i).is_none() {
                     // TODO: occur check
                     self.0.push((*i, t.clone()));
                 } else {
-                    let mut u = Type::Var(*i);
-                    u.apply_subst(self);
+                    let mut u = Type::Mvar(*i);
+                    self.apply_type(&mut u);
                     self.unify(&u, t);
                 }
             }
@@ -265,31 +283,24 @@ impl TypeSubst {
         }
     }
 
-    fn infer(&mut self, m: &mut Term, env: &mut HashMap<String, (HashSet<usize>, Type)>) {
+    fn infer(&mut self, m: &mut Term, env: &mut HashMap<String, TypeScheme>) {
         match m {
             Term::Fvar(t, name) => {
                 let u = match env.get(name) {
                     None => unimplemented!("{}", name),
-                    Some((vars, t)) => {
-                        // rename schematic type variables
-                        let mut u = t.clone();
-                        for s in vars {
-                            u.subst(*s, &Type::Var(fresh_tvar()));
-                        }
-                        u
-                    }
+                    Some(s) => s.instantiate(),
                 };
                 self.unify(t, &u);
             }
             Term::Bvar(_, _) => unimplemented!(),
             Term::Abs(t, m) => {
-                let fresh_tv = fresh_tvar();
+                let mid = fresh_mvar();
                 self.unify(
                     t,
-                    &Type::Arrow(Box::new(Type::Var(fresh_tv)), Box::new(m.r#type().clone())),
+                    &Type::Arrow(Box::new(Type::Mvar(mid)), Box::new(m.r#type().clone())),
                 );
                 let name = fresh();
-                env.insert(name.clone(), (Default::default(), Type::Var(fresh_tv)));
+                env.insert(name.clone(), TypeScheme(0, Type::Mvar(mid)));
                 m.open(&name);
                 self.infer(m, env);
                 m.close(&name);
@@ -308,10 +319,10 @@ impl TypeSubst {
 }
 
 impl Term {
-    pub fn infer(&mut self, env: &HashMap<String, (HashSet<usize>, Type)>) {
+    pub fn infer(&mut self, env: &HashMap<String, TypeScheme>) {
         let mut subst = TypeSubst::default();
         subst.infer(self, &mut env.clone());
-        self.apply_subst_type(&subst)
+        subst.apply_term(self);
     }
 }
 
@@ -323,28 +334,28 @@ mod tests {
     fn test_infer() {
         let env = vec![(
             "f".to_owned(),
-            (
-                Default::default(),
+            TypeScheme(
+                0,
                 Type::Arrow(
-                    Type::Base("a".to_owned()).into(),
-                    Type::Base("b".to_owned()).into(),
+                    Type::Fvar("a".to_owned()).into(),
+                    Type::Fvar("b".to_owned()).into(),
                 ),
             ),
         )]
         .into_iter()
         .collect();
         let mut m = Term::App(
-            Type::Var(fresh_tvar()),
-            Term::Fvar(Type::Var(fresh_tvar()), "f".to_owned()).into(),
-            Term::Bvar(Type::Var(fresh_tvar()).into(), 0).into(),
+            Type::Mvar(fresh_mvar()),
+            Term::Fvar(Type::Mvar(fresh_mvar()), "f".to_owned()).into(),
+            Term::Bvar(Type::Mvar(fresh_mvar()).into(), 0).into(),
         )
-        .into_abs("x", Type::Var(fresh_tvar()));
+        .into_abs("x", Type::Mvar(fresh_mvar()));
         m.infer(&env);
         assert_eq!(
             m.r#type(),
             &Type::Arrow(
-                Type::Base("a".to_owned()).into(),
-                Type::Base("b".to_owned()).into()
+                Type::Fvar("a".to_owned()).into(),
+                Type::Fvar("b".to_owned()).into()
             )
         );
     }
